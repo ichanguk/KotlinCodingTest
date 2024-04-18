cebe3d25ccb256c864898ac98439faf4f2b754a9
changukLee <ckddnr5527@gmail.com>
2024-04-18 21:53:44 +0900

    feat: 입력과 계산_나누기 update

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/입력과계산/나누기.kt
@@ -0,0 +1,12 @@
+package com.example.kotlincodingtest.baekjoon.입력과계산
+
+import java.io.BufferedReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val st = StringTokenizer(readLine())
+    val A = st.nextToken().toDouble()
+    val B = st.nextToken().toInt()
+
+    println(A / B)
+}
