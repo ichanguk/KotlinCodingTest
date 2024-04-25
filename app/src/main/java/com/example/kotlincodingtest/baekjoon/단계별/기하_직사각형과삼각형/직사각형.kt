23bcdfb2bdc97fc9040e81272fb1fedab31e32ff
changukLee <ckddnr5527@gmail.com>
2024-04-25 17:06:02 +0900

    Update 직사각형과삼각형_직사각형

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/기하_직사각형과삼각형/직사각형.kt
@@ -0,0 +1,10 @@
+package com.example.kotlincodingtest.baekjoon.단계별.기하_직사각형과삼각형
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val A = readLine().toInt()
+    val B = readLine().toInt()
+
+    println(A * B)
+}
