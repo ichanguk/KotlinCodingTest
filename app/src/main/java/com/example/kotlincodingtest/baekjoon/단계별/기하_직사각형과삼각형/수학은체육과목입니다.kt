1ee071c36854cc2e03735ad38613b6059c20519b
changukLee <ckddnr5527@gmail.com>
2024-04-25 17:08:48 +0900

    Update 직사각형과삼각형_수학은체육과목입니다

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/기하_직사각형과삼각형/수학은체육과목입니다.kt
@@ -0,0 +1,8 @@
+package com.example.kotlincodingtest.baekjoon.단계별.기하_직사각형과삼각형
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val n = readLine().toLong()
+    println(n * 4)
+}
