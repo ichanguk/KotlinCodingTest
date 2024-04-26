e70dce23a4611f786ff82010e7e75a4e04f9616a
changukLee <ckddnr5527@gmail.com>
2024-04-26 12:17:30 +0900

    Update 정렬_좌표 정렬하기

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/정렬/좌표_정렬하기.kt
@@ -0,0 +1,26 @@
+package com.example.kotlincodingtest.baekjoon.단계별.정렬
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val N = readLine().toInt()
+    val points = MutableList<Pair<Int, Int>>(N) {Pair(0, 0)}
+
+    for (i in 0 until N) {
+        val (x, y) = readLine().split(' ').map { it.toInt() }
+        points[i] = Pair(x, y)
+    }
+
+    points.sortWith (
+        compareBy<Pair<Int, Int>> {
+            it.first
+        }.thenBy {
+            it.second
+        }
+    )
+
+    points.forEach {
+        println("${it.first} ${it.second}")
+    }
+
+}
