64a6d94d0d3f8e4b1ebb615d5fd0b626e1f23e5c
changukLee <ckddnr5527@gmail.com>
2024-05-01 11:59:54 +0900

    Update 조합론_녹색거탑

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/조합론/녹색거탑.kt
@@ -0,0 +1,7 @@
+package com.example.kotlincodingtest.baekjoon.단계별.조합론
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    println( Math.pow(2.0, readLine().toDouble()).toInt())
+}
