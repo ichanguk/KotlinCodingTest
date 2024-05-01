420c1b117b3b68a156c3f3cc9a6990cebaa7fe47
changukLee <ckddnr5527@gmail.com>
2024-05-01 12:12:01 +0900

    Update 조합론_이항계수1

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/조합론/이항계수1.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/조합론/이항계수1.kt
@@ -3,9 +3,15 @@ package com.example.kotlincodingtest.baekjoon.단계별.조합론
 import java.io.BufferedReader
 
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
-    val N = readLine().toInt()
-    println(fact(N))
+    val (N, K) = readLine().split(' ').map { it.toInt() }
+
+    println(combi(N, K))
 }
+
+fun combi(n: Int, k: Int): Int {
+    return fact(n) / fact(n - k) / fact(k)
+}
+
 
 private fun fact(n: Int): Int {
     var res = 1
