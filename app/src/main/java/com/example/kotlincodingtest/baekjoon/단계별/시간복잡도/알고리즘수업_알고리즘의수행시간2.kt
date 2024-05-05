42eafcdeca5584464d8861fe3c08163e2a67bbe0
changukLee <ckddnr5527@gmail.com>
2024-05-05 21:18:58 +0900

    Update 시간복잡도_알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/시간복잡도/알고리즘수업_알고리즘의수행시간2.kt
@@ -0,0 +1,8 @@
+package com.example.kotlincodingtest.baekjoon.단계별.시간복잡도
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    println(readLine())
+    println(1)
+}
