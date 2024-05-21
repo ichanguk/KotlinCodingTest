93252dd0653a1256c1080085ab0d04efc9395515
changukLee <ckddnr5527@gmail.com>
2024-05-21 14:13:30 +0900

    Update 우선 순위 큐_절댓값 힙

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/우선순위큐/절댓값_힙.kt
@@ -0,0 +1,27 @@
+package com.example.kotlincodingtest.baekjoon.단계별.우선순위큐
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.PriorityQueue
+import kotlin.math.abs
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val pq = PriorityQueue<Int>(compareBy<Int> { abs(it) }.thenComparing(compareBy<Int> { it }))
+    val N = readLine().toInt()
+    var cmd: Int
+    val bw = BufferedWriter(System.out.bufferedWriter())
+    for (i in 1..N) {
+        cmd = readLine().toInt()
+        if (cmd == 0) {
+            if (pq.isNotEmpty()) {
+                bw.write("${pq.poll()}\n")
+            } else {
+                bw.write("0\n")
+            }
+        } else {
+            pq.add(cmd)
+        }
+    }
+    bw.flush()
+    bw.close()
+}
