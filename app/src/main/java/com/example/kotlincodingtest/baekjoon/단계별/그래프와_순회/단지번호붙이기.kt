7c40672095aec5b60c809fcd440d138c9fd5af9b
changukLee <ckddnr5527@gmail.com>
2024-05-25 17:24:34 +0900

    Update 그래프와 순회_단지번호붙이기

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/단계별/그래프와_순회/단지번호붙이기.kt
@@ -0,0 +1,61 @@
+package com.example.kotlincodingtest.baekjoon.단계별.그래프와_순회
+
+import java.io.BufferedReader
+
+private var isVisit = mutableListOf(mutableListOf<Boolean>())
+private var houseMap = mutableListOf(mutableListOf<Int>())
+private var N = 0
+private var cnt = 0
+
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    N = readLine().toInt()
+    houseMap = MutableList(N) { MutableList(N) { 0 } }
+    isVisit = MutableList(N) { MutableList(N) { false } }
+    var input: String
+    for (i in 0 until N) {
+        input = readLine()
+        for (j in 0 until N) {
+            houseMap[i][j] = input[j] - '0'
+            if (houseMap[i][j] == 0) {
+                isVisit[i][j] = true
+            }
+        }
+    }
+
+    var totalCnt = 0
+    var cntList = mutableListOf<Int>()
+    for (i in 0 until N) {
+        for (j in 0 until N) {
+            if (!isVisit[i][j]) {
+                cnt = 0
+                dfs(i, j)
+                totalCnt++
+                cntList.add(cnt)
+            }
+        }
+    }
+
+    println(totalCnt)
+    cntList.sorted().forEach {
+        println(it)
+    }
+
+}
+
+private val dx = mutableListOf(-1, 0, 1, 0)
+private val dy = mutableListOf(0, 1, 0, -1)
+
+private fun dfs(x:Int, y:Int) {
+    cnt++
+    isVisit[x][y] = true
+    var nx:Int
+    var ny:Int
+    for (i in 0 until 4) {
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (nx in 0 until N && ny in 0 until N && !isVisit[nx][ny]) {
+            dfs(nx, ny)
+        }
+    }
+}
