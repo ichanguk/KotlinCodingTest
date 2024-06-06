6264c3012b3be5707789ed61b646a31cccf6adcf
changukLee <ckddnr5527@gmail.com>
2024-06-06 14:59:19 +0900

    Update Lv1.덧칠하기

---
+++ app/src/main/java/com/example/kotlincodingtest/programmers/Lv1/구현/덧칠하기/Solution.kt
@@ -0,0 +1,24 @@
+package com.example.kotlincodingtest.programmers.Lv1.구현.덧칠하기
+
+class Solution {
+    fun solution(n: Int, m: Int, section: IntArray): Int {
+        var answer: Int = 0
+        var wall = MutableList(n + 1) { 0 }
+        for (s in section) {
+            wall[s] = 1
+        }
+
+        var i = 1
+        while (i <= n) {
+            if (wall[i] == 1) {
+                answer++
+                i += m
+            } else {
+                i ++
+            }
+        }
+
+
+        return answer
+    }
+}
