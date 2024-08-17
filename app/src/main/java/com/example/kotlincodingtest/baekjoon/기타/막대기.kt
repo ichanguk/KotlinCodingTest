298b0b06aebf98a4745fde773a671307f40c6886
changukLee <ckddnr5527@gmail.com>
2024-08-17 19:31:08 +0900

    Update 기타. 막대기

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/막대기.kt
@@ -0,0 +1,9 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val N = readLine().toInt().toString(2)
+
+    println(N.count{it == '1'})
+}
