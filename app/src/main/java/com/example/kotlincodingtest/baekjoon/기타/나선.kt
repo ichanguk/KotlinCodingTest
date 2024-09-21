7405ee307b23b82efd858dae70ef9862cf924dab
changuk Lee <ckddnr5527@gmail.com>
2024-09-22 00:10:58 +0900

    Update 기타. 나선

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/나선.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/나선.kt
@@ -1,40 +1,29 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
 import java.io.BufferedReader
 
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
     val (M, N) = readLine().split(' ').map { it.toInt() }
-    val isVisit = Array(N) { BooleanArray(M) { false } }
 
-    val dq = ArrayDeque<Pair<Int, Int>>()
-    dq.addLast(Pair(N - 1, 0))
-    isVisit[N - 1][0] = true
-
-    val dx = arrayOf(0, -1, 0, 1)
-    val dy = arrayOf(1, 0, -1, 0)
-
-    var dir = 0
-    var nx = 0
-    var ny = 0
-    var cnt = 1
-
-    while (dq.isNotEmpty() ) {
-        val cur = dq.removeFirst()
-
-        if (cnt == N * M) {
-            println("${cur.second} ${N - (cur.first + 1)}")
-            break
-        }
-        nx = cur.first + dx[dir]
-        ny = cur.second + dy[dir]
-
-        if (!(nx in 0 until N && ny in 0 until M && !isVisit[nx][ny])) {
-            dir = (dir + 1) % 4
-            nx = cur.first + dx[dir]
-            ny = cur.second + dy[dir]
-        }
-
-        dq.addLast(Pair(nx, ny))
-        isVisit[nx][ny] = true
-        cnt++
+    val minSize = minOf(M, N)
+    var outside = minSize / 2
+    if (minSize % 2 == 0) {
+        outside--
     }
+
+    val m = M - outside * 2
+    val n = N - outside * 2
+
+    var x = outside
+    var y = outside
+
+    if (n == 1 && m > n) {
+        x = M - outside - 1
+    }
+    if (m == 2 && n > m) {
+        y = N - outside - 2
+    }
+
+    println("$x ${N - (y + 1)}")
 
 }
