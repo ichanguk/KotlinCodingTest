c4df3ee18f975834115800017f71ea2e2ba55f80
changuk Lee <ckddnr5527@gmail.com>
2025-02-06 22:55:45 +0900

    Update 기타. 닉네임에 갓 붙이기

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/닉네임에_갓_붙이기.kt
@@ -0,0 +1,11 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val n = readLine().toInt()
+
+    repeat(n) {
+        println("god" + readLine().split(' ').drop(1).joinToString(""))
+    }
+}
