977d5265c341a9ba725dbcfd6d5fb9d8b5e550c8
changuk Lee <ckddnr5527@gmail.com>
2025-02-11 00:01:47 +0900

    Update 기타. 히든 넘버

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/히든_넘버.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/히든_넘버.kt
@@ -4,7 +4,7 @@ import java.io.BufferedReader
 
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
     val n = readLine().toInt()
-    val hiddenNum = readLine().split(Regex("\\D+")).filter { it.isNotEmpty() }.map { it.toInt() }
+    val hiddenNum = readLine().split(Regex("\\D+")).filter { it.isNotEmpty() }.map { it.toLong() }
 
     println(hiddenNum.sum())
 }
