c6a16d20282557fb173ac04d3c357d52dc0f8f02
changuk Lee <ckddnr5527@gmail.com>
2025-03-01 00:09:30 +0900

    Update 기타. 생일

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/생일.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/생일.kt
@@ -3,5 +3,17 @@ package com.example.kotlincodingtest.baekjoon.기타
 import java.io.BufferedReader
 
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val n = readLine().toInt()
+    val birthday = mutableListOf<Pair<Int, String>>()
 
+    repeat(n) {
+        val (name, d, m, y) = readLine().split(' ')
+
+        birthday.add(Pair(d.toInt() + (m.toInt() * 12) + (y.toInt() * 365), name))
+
+        birthday.sortBy { it.first }
+    }
+
+    println(birthday[n - 1].second)
+    println(birthday[0].second)
 }
