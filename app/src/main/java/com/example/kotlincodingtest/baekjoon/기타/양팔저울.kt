6c5aa9ffebcd9e99d79335209c87d3a829243aed
changuk Lee <102301656+ichanguk@users.noreply.github.com>
2025-03-05 18:03:57 +0900

    Update 기타. 양팔저울

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/양팔저울.kt
@@ -0,0 +1,8 @@
+import java.io.*
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val n = readLine().toInt()
+    val weights = readLine().split(' ').map{it.toInt()}
+    
+    println()
+}
