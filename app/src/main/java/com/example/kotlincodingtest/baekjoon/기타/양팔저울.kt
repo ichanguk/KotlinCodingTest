d8ebfcad117706d83bbad9a4c63323dc1cf8b5dc
changuk Lee <ckddnr5527@gmail.com>
2025-03-07 20:36:44 +0900

    Update 기타. 양팔저울

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/양팔저울.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/양팔저울.kt
@@ -1,8 +1,36 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
 import java.io.*
+import kotlin.math.abs
 
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
     val n = readLine().toInt()
     val weights = readLine().split(' ').map{it.toInt()}
+
+    var l = weights[0]
+    var r = weights[1]
+
+    for (i in 2 until n) {
+        if (l <= r) {
+            l += weights[i]
+        } else {
+            r += weights[i]
+        }
+    }
+
+    var diff = abs(l -r)
+    var ans = 0
+
+    val plusW = intArrayOf(100, 50, 20, 10, 5, 2, 1)
+
+    for (w in plusW) {
+        if (diff == 0) {
+            break
+        }
+
+        ans += diff / w
+        diff %= w
+    }
     
-    println()
+    println(ans)
 }
