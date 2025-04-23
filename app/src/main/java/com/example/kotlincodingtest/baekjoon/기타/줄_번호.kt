44053b8df9b9d19f94aa75acf31a0bb44e2d323e
changuk Lee <ckddnr5527@gmail.com>
2025-04-23 22:40:58 +0900

    Update 기타. 줄 번호

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/줄_번호.kt
@@ -0,0 +1,12 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val n = readLine().toInt()
+    repeat(n) {
+        val input = readLine()
+
+        println("${it + 1}. " + input)
+    }
+}
