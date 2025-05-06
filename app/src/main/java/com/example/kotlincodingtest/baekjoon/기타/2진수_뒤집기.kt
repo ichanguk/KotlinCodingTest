8858d913869aabfa157f274a044342180dd89bb1
changuk Lee <ckddnr5527@gmail.com>
2025-05-06 23:02:35 +0900

    Update 기타. 2진수 뒤집기

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/2진수_뒤집기.kt
@@ -0,0 +1,10 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val n = readLine().toLong().toString(2).reversed().toLong(2)
+
+    println(n)
+
+}
