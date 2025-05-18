a643cf23979b5554a017a33b3a5d9115921850fd
changuk Lee <ckddnr5527@gmail.com>
2025-05-18 22:01:14 +0900

    Update 기타. 별 찍기 17

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/별_찍기_17.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/별_찍기_17.kt
@@ -5,5 +5,21 @@ import java.io.BufferedReader
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
     val n = readLine().toInt()
 
-    
+    repeat(n - 1) {
+        repeat(n - 1 - it) {
+            print(' ')
+        }
+        print('*')
+        repeat(2 * it - 1) {
+            print(' ')
+        }
+        if (it > 0) {
+            print('*')
+        }
+        println()
+    }
+    repeat(2 * n - 1) {
+        print('*')
+    }
+    println()
 }
