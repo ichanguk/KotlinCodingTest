f441f0e06a3d716def1f505596f756ab64524300
changuk Lee <ckddnr5527@gmail.com>
2025-05-23 18:05:03 +0900

    Update 기타. AI 선도대학

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/AI선도대학.kt
@@ -0,0 +1,7 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    println(readLine().toInt() - 2024)
+}
