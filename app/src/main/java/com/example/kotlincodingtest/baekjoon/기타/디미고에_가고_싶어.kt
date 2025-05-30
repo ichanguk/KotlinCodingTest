134376f71b5336ec0f11f6547f76949bdcbc38d4
changuk Lee <ckddnr5527@gmail.com>
2025-05-30 23:59:41 +0900

    Update 기타. 디미고에 가고싶어

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/디미고에_가고_싶어.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/디미고에_가고_싶어.kt
@@ -5,5 +5,10 @@ import java.io.BufferedReader
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
     val (a, b) = readLine().split(' ').map { it.toInt() }
 
-    println((b - a) / 2 + 1)
+    var ans = (b - a) / 2 + 1
+    if (a % 2 == 0 && b % 2 == 1) {
+        ans++
+    }
+
+    println(ans)
 }
