e7e6f3f2fb5eeb34d4b0eb46072fb679f2a02a58
changuk Lee <ckddnr5527@gmail.com>
2025-06-20 22:47:18 +0900

    Update 기타. 사랑은 고려대입니다

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/사랑은_고려대입니다.kt
@@ -0,0 +1,11 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    val n = readLine().toInt()
+
+    repeat(n) {
+        print("LoveisKoreaUniversity ")
+    }
+}
