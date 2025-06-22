44da13a9571951d4bdff0941ac07d28639b62490
changuk Lee <ckddnr5527@gmail.com>
2025-06-22 21:47:20 +0900

    Update 기타. 나는 행복합니다

--- app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/나는_행복합니다.kt
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/나는_행복합니다.kt
@@ -5,5 +5,5 @@ import java.io.BufferedReader
 fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
     val (a, b, c) = readLine().split(' ').map { it.toInt() }
 
-    println("${c / b} ${c / a}")
+    println("${c / b} ${c % b}")
 }
