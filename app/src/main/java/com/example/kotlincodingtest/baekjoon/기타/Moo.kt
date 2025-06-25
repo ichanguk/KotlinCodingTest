1251703c608d42b2389d73e997f39244c4790019
changuk Lee <ckddnr5527@gmail.com>
2025-06-25 20:58:52 +0900

    Update 기타. Moo

---
+++ app/src/main/java/com/example/kotlincodingtest/baekjoon/기타/Moo.kt
@@ -0,0 +1,13 @@
+package com.example.kotlincodingtest.baekjoon.기타
+
+import java.io.BufferedReader
+
+fun main() = with(BufferedReader(System.`in`.bufferedReader())) {
+    println("(___)")
+    println("(o o)____/")
+    println(" @@      \\")
+    println("  \\ ____,/")
+    println("  //   //")
+    println(" ^^   ^^")
+
+}
